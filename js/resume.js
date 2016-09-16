dd5ae399aa7940d5f5d78ff511cfe9286513d5fb
Nick Place <nickjplace@gmail.com>
2016-09-16 15:02:29 -0600

    edits

--- js/resume.js
+++ js/resume.js
@@ -18,7 +18,7 @@ $(function() {
     "In May, I graduated with an HCI degree from Colorado State University. Our final project was an <a href='https://www.dropbox.com/s/elhq6hf3uzhvd9y/Part%203%20Final%20Report.docx?dl=0'>app for doormen</a>.", 
     "In January, I designed & built an e-learning <a href='http://ironwomanstrength.com'>product</a> for Leanna Carr.", 
     "In 2014 and 15, I worked with Ray on an e-tutoring <a href='http://chemwithray.com'>system</a> built to supplement university chemistry.", 
-    "Early 2014, my internship with <a href='http://tackmobile.com'>Tack Mobile</a> ended. During it, I got to collaborate with some super talented folks. I helped ship <a href='http://tackmobile.com/work/start'>Start</a>, <a href='http://takenoted.com'>Noted</a> and <a href='https://itunes.apple.com/us/app/denver-rail/id523272854?mt=8'>Denver Rail</a>.", 
+    "In 2013 and '14 I worked with some super talented folks at <a href='http://tackmobile.com'>Tack Mobile</a> on <a href='http://tackmobile.com/work/start'>Start</a>, <a href='http://takenoted.com'>Noted</a> and <a href='https://itunes.apple.com/us/app/denver-rail/id523272854?mt=8'>Denver Rail</a>.", 
     "In 2013, I designed a logo and a landing page for the climbing accessory, <a href='https://vimeo.com/88199986'>Cam Keeper</a>.", 
     "Before that, I was diving into books about PHP, color theory, cybernetics and branding. I also maintained a flash gaming <a href='https://web.archive.org/web/20110202174521/http://relaxist.com/'>website</a> with a buddy."
     ];
